53584efe9bd80503b5f708bbf35e62b80363b0d6
YacineHekkas <yac.hakkas@gmail.com>
2024-11-12 19:35:48 +0100

    login & signup screens

---
+++ lib/view/screens/profil/legal_policy_view.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shopease_ecom_app/data/const/constants.dart';
+
+
+class LegalPolicyView extends StatelessWidget {
+
+  String dummyTextDescription = "";
+   LegalPolicyView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Legal and Policy',
+          style: AppTypography.kSemiBold16.copyWith(color: AppColors.kGrey100),
+        ),
+      ),
+      body: RawScrollbar(
+        thumbColor: AppColors.kPrimary,
+        thumbVisibility: true,
+        radius: const Radius.circular(20),
+        thickness: 5,
+        child: ListView(
+          physics: const BouncingScrollPhysics(),
+          padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 20.h),
+          children: [
+            Text(
+              'Terms',
+              style: AppTypography.kSemiBold16,
+            ),
+            SizedBox(height: AppSpacing.tenVertical),
+            Text(
+              '$dummyTextDescription\n\n$dummyTextDescription',
+              textAlign: TextAlign.justify,
+              style: AppTypography.kMedium14
+                  .copyWith(color: AppColors.kGrey80, fontSize: 14.sp),
+            ),
+            SizedBox(height: AppSpacing.tenVertical),
+            Text(
+              'Changes to the Service and/or Terms:',
+              style: AppTypography.kSemiBold16,
+            ),
+            SizedBox(height: AppSpacing.tenVertical),
+            Text(
+              '$dummyTextDescription\n\n$dummyTextDescription\n\n$dummyTextDescription\n\n$dummyTextDescription',
+              textAlign: TextAlign.justify,
+              style: AppTypography.kMedium14.copyWith(color: AppColors.kGrey80),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
